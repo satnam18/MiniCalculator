31e0d10a0e5d4b47b5480de2c8b7dfb49f6c2e94
Priya Gupta <priya.gupta2014@vit.ac.in>
2018-12-02 12:44:56 +0530

    Code refactored

--- index.js
+++ index.js
@@ -16,26 +16,31 @@ function divide(number1, number2) {
   return number2 !== 0 ? number1 / number2 : 'Denominator can not be zero';
 }
 
-do {
-  console.log(' \n \n Welocme to Mini calculator');
-  console.log(' \nEnter the 2 values you want to have operations on :');
-  const firstNumber = parseFloat(readline.question('First Value : '), 10);
-  const secondNumber = parseFloat(readline.question('Second Value : '), 10);
+function main() {
+  let choice;
+  do {
+    console.log(' \n \n Welocme to Mini calculator');
+    console.log(' \nEnter the 2 values you want to have operations on :');
+    const firstNumber = parseFloat(readline.question('First Value : '), 10);
+    const secondNumber = parseFloat(readline.question('Second Value : '), 10);
 
-  console.log(' \n\n Press 1 for Addition \n Press 2 for Subtraction \n Press 3 for Multiplication \n Press 4 for Division');
-  const choice = parseInt(readline.question(), 10);
-  let result = 0;
+    console.log(' \n\n Press 1 for Addition \n Press 2 for Subtraction \n Press 3 for Multiplication \n Press 4 for Division');
+    choice = parseInt(readline.question(), 10);
+    let result = 0;
 
-  if (choice === 1) {
-    result = add(firstNumber, secondNumber);
-  } else if (choice === 2) {
-    result = subtract(firstNumber, secondNumber);
-  } else if (choice === 3) {
-    result = multiply(firstNumber, secondNumber);
-  } else if (choice === 4) {
-    result = divide(firstNumber, secondNumber);
-  }
+    if (choice === 1) {
+      result = add(firstNumber, secondNumber);
+    } else if (choice === 2) {
+      result = subtract(firstNumber, secondNumber);
+    } else if (choice === 3) {
+      result = multiply(firstNumber, secondNumber);
+    } else if (choice === 4) {
+      result = divide(firstNumber, secondNumber);
+    }
 
 
-  console.log('​The result after the operatiion is : ', result);
-} while (true);
+    console.log('​The result after the operatiion is : ', result);
+    choice = readline.question('Do you want to continue y/n ?');
+  } while (choice === 'y' || choice === 'Y');
+}
+main();
